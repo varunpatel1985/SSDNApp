1c3bd645164ea41344b5acd4c76255e187a761b5
varun patel <varunpatel1985@gmail.com>
2021-07-19 17:14:13 +1000

    test commit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -17,7 +17,7 @@ namespace API.Controllers
         {
             _context = context;
         }
-
+        //test
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
         {
